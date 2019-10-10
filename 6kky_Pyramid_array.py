cad65d55379e9b32c3840139e7233056e5d2f788
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-10-10 17:26:26 +0500

    6

--- 6kky_Pyramid_array.py
+++ 6kky_Pyramid_array.py
@@ -4,12 +4,5 @@ pyramid(2) => [ [1], [1, 1] ]
 pyramid(3) => [ [1], [1, 1], [1, 1, 1] ] """
 
 def pyramid(n):
-    result = []
-    
-    for i in range(n):
-        result.append([1])
-        for j in range(i):
-            result[i].append(1)              
-
-    return result
+    return [[1]*x for x in range(1, n+1)]
 
