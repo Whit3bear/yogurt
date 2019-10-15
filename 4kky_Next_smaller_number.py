f2158bba3ad0e14509aab652b4d5a8a238caa006
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-10-15 18:59:23 +0500

    777

--- 4kky_Next_smaller_number.py
+++ 4kky_Next_smaller_number.py
@@ -1,37 +1,31 @@
 def next_smaller(n):  
-    
-    lst = [int(x) for x in str(n)]
-    for i in range(len(lst)-1, 0, -1):     
-        
+    lst = [int(x) for x in str(n)]    
+    for i in range(len(lst)-1, 0, -1):                            
         if lst[i-1] > lst[i]:                        
-            break
-    # ничего не найдено это минимальное число
+            break            
     else:        
         return -1
 
-    swap = i    
-    # цикл по срезу правой части списка, ищем элемент который меньше числа не по порядку, но больше числа идущего за ним в списке   
+    swap = i        
     for j in range(i, len(lst)):        
         if lst[j] < lst[i-1] and lst[j] > lst[i]:        
             swap = j
+        
+    lst[swap], lst[i-1] = lst[i-1], lst[swap]           
     
-    # выполняем замену
-    lst[swap], lst[i-1] = lst[i-1], lst[swap]
-
-    #сортируем
     newlst = lst[:i] + sorted(lst[i:], reverse=True)
-    return newlst
+    
+    if newlst[0] == 0:
+        return -1
+    
+    return int(''.join([str(x) for x in newlst]))
 
-    # 24135 свапаем 4 и 1, 21435 находим наименьший номер, с которым меняемся значением, далее сортируем после 4 последние две цифры по убыванию, 21453
-    # 24325
-
-# ноль нужно игнорировать, брать предыдущее значение
+#print(next_smaller(1207))
+# 1072
 print(next_smaller(1027))
 # -1
-# должно быть меньше 5 [i-1] но больше 1 [i]
 #print(next_smaller(513))
 # 351
-
 #print(next_smaller(907))
 #790
 #print(next_smaller(5311))
