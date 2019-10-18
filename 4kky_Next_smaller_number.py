b9d2ab6eb199dac91af583db35490ee73d8f1587
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-10-18 17:39:56 +0500

    777

--- 4kky_Next_smaller_number.py
+++ 4kky_Next_smaller_number.py
@@ -24,23 +24,3 @@ def next_smaller(n):
 # 1072
 print(next_smaller(1027))
 # -1
-#print(next_smaller(513))
-# 351
-#print(next_smaller(907))
-#790
-#print(next_smaller(5311))
-#5131
-#print(next_smaller(135))
-#-1
-#print(next_smaller(2071))
-#2017
-#print(next_smaller(2415))
-#2154
-#print(next_smaller(415))
-#154
-#print(next_smaller(123456798))
-#123456789
-#next_smaller(123456789)
-#-1)
-#print(next_smaller(1234567908))
-#1234567890
