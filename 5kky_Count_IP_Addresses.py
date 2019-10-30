6a38ba9431f1783c2d009dea3c90c25bf4eb6480
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-10-30 17:36:52 +0500

    1

---
+++ 5kky_Count_IP_Addresses.py
@@ -0,0 +1,7 @@
+def ips_between(start, end):
+    
+    return
+
+ultratest.assert_equals(ips_between("10.0.0.0", "10.0.0.50"), 50)
+# ips_between("10.0.0.0", "10.0.1.0")   ==  256 
+# ips_between("20.0.0.10", "20.0.1.0")  ==  246
