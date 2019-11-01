bd5397845abadbca693103964a6a67aff7c70072
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-11-01 17:12:46 +0500

    7

--- 4kky_Shortest_Knight_Path.py
+++ 4kky_Shortest_Knight_Path.py
@@ -1,38 +1,44 @@
-def knight(p1, p2):
-    currentpos_x = p1[0]
-    currentpos_y = p1[1]
+class cell:
+    def __init__(self, x = 0, y = 0, dist = 0):
+        self.x = x
+        self.y = y
+        self.dist = dist
 
-    counter = 0
+def isInside(x, y, N):
+    if (x >= 1 and x <= N and y >= 1 and y <= N):
+        return True
+    return False
 
-    if p1 == p2:
-        return counter
-    else:
-        counter += 1   
-           
-    nextposition_x = ord(currentpos_x) + 1
-    nextposition_y = int(currentpos_y) + 2
+def minStepToReachTarget(knightpos, targetpos, N):
+        dx = [2, 2, -2, -2, 1, 1, -1, -1]
+        dy = [1, -1, 1, -1, 2, -2, 2, -2]
 
-    if 96 < nextposition_x < 103 and 0 < nextposition_y < 9:
-        return knight(chr(nextposition_x) + str(nextposition_y), p2)
+        queue = []
 
-    nextposition_x = ord(currentpos_x) + 2
-    nextposition_y = int(currentpos_y) + 1
+        queue.append(cell(knightpos[0], knightpos[1], 0))
 
-    if 96 < nextposition_x < 103 and 0 < nextposition_y < 9:
-        return knight(chr(nextposition_x) + str(nextposition_y), p2)
+        visited = [[False for i in range(N+1)] for j in range(N + 1)]
 
-    nextposition_x = ord(currentpos_x) - 1
-    nextposition_y = int(currentpos_y) - 2
+        visited[knightpos[0]][knightpos[1]] = True
 
-    if 96 < nextposition_x < 103 and 0 < nextposition_y < 9:
-        return knight(chr(nextposition_x) + str(nextposition_y), p2)
+        while(len(queue) > 0):
+            t = queue[0]
+            queue.pop(0)
 
-    nextposition_x = ord(currentpos_x) - 2
-    nextposition_y = int(currentpos_y) - 1
+            if(t.x == targetpos[0] and t.y == targetpos[1]):
+                return t.dist
 
-    if 96 < nextposition_x < 103 and 0 < nextposition_y < 9:
-        return knight(chr(nextposition_x) + str(nextposition_y), p2)   
-    
+            for i in range(8):
+                x = t.x + dx[i]
+                y = t.y + dy[i]
 
+                if (isInside(x, y, N) and not visited[x][y]):
+                    visited[x][y] = True
+                    queue.append(cell(x, y, t.dist + 1))
+
+def knight(startpos, targetpos):
+    arr_startpos = [ord(startpos[0]), int(startpos[1])]
+    arr_targetpos = [ord(targetpos[0]), int(targetpos[1])]  
+    return minStepToReachTarget(arr_startpos, arr_targetpos, 8)
 #arr = [['a1', 'c1', 2], ['a1', 'f1', 3], ['a1', 'f3', 3], ['a1', 'f4', 4], ['a1', 'f7', 5]]
-print(knight('a1','b1'))
+print(knight('a1','c1'))
