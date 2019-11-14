70061913e9a5706c24c4daab68c7a1f6b2dc31fe
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-11-14 14:55:17 +0500

    1

--- ultra_test.py
+++ ultra_test.py
@@ -1,5 +1,10 @@
-def assert_equals(a, b):
-    if a == b:
-        print('successful')
-    else:
-        print('{} is not equal {}'.format(a, b))
+import unittest
+
+# def assert_equals(a, b):
+#     if a == b:
+#         print('successful')
+#     else:
+#         print('{} is not equal {}'.format(a, b))
+
+d = {1: 3, 4: 6}
+print(d.get(7, 10))
