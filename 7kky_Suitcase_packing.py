6b202b9a28d1d65c6bc4d15779bb92203cc5e8f4
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-11-22 16:27:58 +0500

    1

--- 7kky_Suitcase_packing.py
+++ 7kky_Suitcase_packing.py
@@ -1,6 +1,2 @@
-def fit_in(a,b,m,n):
-    print(a,b,m,n)
-    print(m*n, a*b*2)
-    return m*n>a*b*2
-    
-print(fit_in(7, 1, 7, 8))
+def fit_in(a, b, m, n):
+  return max(a, b) <= min(m, n) and a + b <= max(m, n)
