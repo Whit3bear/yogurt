d53ba0291769045248a16a4968fd74b01bec2afc
Whit3bear <55454147+Whit3bear@users.noreply.github.com>
2019-12-19 13:53:12 +0500

    1

--- 6kky_Longest_Alphbetical_substring.py
+++ 6kky_Longest_Alphbetical_substring.py
@@ -1,22 +1,41 @@
+""" Find the longest substring in alphabetical order.
+
+Example: the longest alphabetical substring in "asdfaaaabbbbcttavvfffffdf" is "aaaabbbbctt".
+
+There are tests with strings up to 10 000 characters long so your code will need to be efficient.
+
+The input will only consist of lowercase characters and will be at least one letter long.
+
+If there are multiple solutions, return the one that appears first.
+
+Good luck :)
+ """
+
 def longest(s):
-    m = s[0]
-    res = ''
-    for i in range(1, len(s)):        
-        if s[i] < s[i-1]:
-            if len(m) > len(res):
-                res = m
-                m = ''
-        m += s[i]
-    if len(m) > len(res):
-        res = m
-    return res
+    if s <= 1:
+        return s
+    
+    arr = []
+    prev = 0    
+    for i in range(1, len(s)):
+        if s[i-1] > s[i]:
+            arr.append(s[prev:i])
+            prev = i
+        if i == len(s)-1:
+            arr.append(s[prev:])
+    
+    return max(arr, key=len)
+        
+
 
 #print(longest('asd'))
 #'as'
-print(longest('nab'))
+#print(longest('nabc'))
 #'ab'
-# Test.assert_equals(longest('abcdeapbcdef'), 'abcde')
+#print(longest('abcdeapbcdef'))
+#'abcde'
 # Test.assert_equals(longest('asdfaaaabbbbcttavvfffffdf'), 'aaaabbbbctt')
 # Test.assert_equals(longest('asdfbyfgiklag'), 'fgikl')
-# Test.assert_equals(longest('z'), 'z')
+print(longest('z'))
+#'z'
 # Test.assert_equals(longest('zyba'), 'z')
